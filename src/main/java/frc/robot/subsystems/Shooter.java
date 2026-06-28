8b53127f3d700ccd514295b313798b01327adcdb
GokturKardas <gokturkardas@gmail.com>
2026-06-28 14:39:37 +0300

    Shooter için kütüphaneler çağırıldı

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,5 +1,18 @@
 package frc.robot.subsystems;
 
-public class Shooter {
+// Gerekli REV Robotics kütüphanelerini içe aktarıyoruz
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
+
+// Subsystem altyapısı için WPILib kütüphanesi
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+// Port numaralarının tutulduğu Constants dosyasını çağırıyoruz
+import static frc.robot.Constants.ShooterConstants.*;
+
+public class Shooter extends SubsystemBase {
     
-}
